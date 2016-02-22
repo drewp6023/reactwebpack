b43db7048b1b8fdcec38cf6999b065de3876011a
Drew <pickerdr@gmail.com>
2016-02-22 13:10:38 -0700

    New build directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var BUILD_DIR = path.resolve(__dirname, './public');
+var BUILD_DIR = path.resolve(__dirname, './build');
 var APP_DIR = path.resolve(__dirname, './app');
 
 module.exports = {
